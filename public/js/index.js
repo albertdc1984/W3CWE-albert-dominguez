f13f0cd16c90229d63f0b8e846f73b136b13a239
Albert Domínguez <albertdc1984@gmail.com>
2022-01-29 13:58:16 +0100

    Added final line to Image component

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,23 @@
 import PokeCard from "./components/PokeCard.js";
+
 const container = document.querySelector(".poke-cards-container");
 
 new PokeCard(container, "poke-card");
+
 new PokeCard(container, "poke-card");
+
 new PokeCard(container, "poke-card");
+
 new PokeCard(container, "poke-card");
+
 new PokeCard(container, "poke-card");
+
 new PokeCard(container, "poke-card");
+
 new PokeCard(container, "poke-card");
+
 new PokeCard(container, "poke-card");
+
 new PokeCard(container, "poke-card");
+
 new PokeCard(container, "poke-card");
