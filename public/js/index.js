a730d339b63587e4e580528b5bfcf73255244e7f
Albert Domínguez <albertdc1984@gmail.com>
2022-01-29 13:54:56 +0100

    Cards component created & working

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,14 @@
-import Component from "./components/Component.js";
+import PokeCard from "./components/PokeCard.js";
+const container = document.querySelector(".poke-cards-container");
+
+new PokeCard(container, "poke-card");
+new PokeCard(container, "poke-card");
+new PokeCard(container, "poke-card");
+new PokeCard(container, "poke-card");
+new PokeCard(container, "poke-card");
+new PokeCard(container, "poke-card");
+new PokeCard(container, "poke-card");
+new PokeCard(container, "poke-card");
+new PokeCard(container, "poke-card");
+new PokeCard(container, "poke-card");
+console.log("fuck it bro");
