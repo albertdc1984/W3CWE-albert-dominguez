9b305e0610a0c88891616510e3bafef5db460d59
Albert Domínguez <albertdc1984@gmail.com>
2022-01-29 15:17:43 +0100

    Created a function to use const card

--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,8 @@ import PokeCard from "./components/PokeCard.js";
 const container = document.querySelector(".poke-cards-container");
 
 const card = new PokeCard(container, "poke-card");
+
+const showCard = (card) => {
+  return card;
+};
+showCard(card);
