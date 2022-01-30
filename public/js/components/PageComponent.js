40fefd89ad2f42bc03daba0188a48ab3664062da
Albert Domínguez <albertdc1984@gmail.com>
2022-01-30 11:51:41 +0100

    Fixed indentation issue

--- public/js/components/PageComponent.js
+++ public/js/components/PageComponent.js
@@ -21,9 +21,8 @@ class PageComponent extends Component {
       <section class="poke-cards-container"></section>
     </main>
     <footer class="footer-buttons-container">
-     <button class="footer-button button-next">Previous 20</button>
-      <button class="footer-button button-prev">Next 20</button>
-     
+      <button class="footer-button button-next">Previous 20</button>
+      <button class="footer-button button-prev">Next 20</button>     
     </footer>`;
   }
 }
